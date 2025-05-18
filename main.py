936dd43254f18b1d5d9bcb5867d29d7f66c1578b
Behzad Rowshanravan <behr87@hotmail.com>
2025-05-18 15:18:12 +0000

    working version

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from talent_vs_luck.src.utils import (
-    UniformAgent,
-    GaussianAgent
+    MonteCarlo,
+    plot_data,
 )
 import pandas as pd
-import matplotlib.pyplot as plt
+import concurrent.futures
 
 
-num= GaussianAgent()
-pd.Series(data=num.pull()).plot(kind="hist")
-plt.tight_layout()
-plt.show()
+
+agent_type= ["UniformAgent", "GaussianAgent"][0]
+total_number_of_participants= 12000
+required_number_of_participants= 10
+number_of_simulations= 100
+
+
+# #This is for a single run
+# montecarlo= MonteCarlo(
+#     agent_type= agent_type, 
+#     total_number_of_participants= total_number_of_participants,
+#     required_number_of_participants= required_number_of_participants,
+#     ).simulation()
+
+# print(montecarlo)
+
+
+
+args = (agent_type, total_number_of_participants, required_number_of_participants)
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    futures = [executor.submit(MonteCarlo, *args) for _ in range(number_of_simulations)]
+    results = [f.result().simulation() for f in concurrent.futures.as_completed(futures)]
+
+df= pd.DataFrame(data= results).mean()
+
+plot_data(dataframe=df)
--- talent_vs_luck/src/utils.py
+++ talent_vs_luck/src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class UniformAgent:
@@ -11,11 +12,58 @@ class UniformAgent:
 
 
 
-
 class GaussianAgent:
     def __init__(self, loc=0.5, scale=0.1):
         self.loc= loc
         self.scale= scale
     
     def pull(self):
-        return np.random.normal(loc=self.loc, scale=self.scale, size=500)
+        return np.abs(np.random.normal(loc=self.loc, scale=self.scale))
+
+
+
+class MonteCarlo:
+    def __init__(self, agent_type, total_number_of_participants, required_number_of_participants):
+        self.total_number_of_participants= total_number_of_participants
+        self.required_number_of_participants= required_number_of_participants
+        self.percentage_talent_contribution= np.linspace(start= 1, stop= 0.5, num= 10)
+        if agent_type == "UniformAgent":
+            self.agent= [UniformAgent() for _ in range(self.total_number_of_participants)]
+        if agent_type == "GaussianAgent":
+            self.agent= [GaussianAgent() for _ in range(self.total_number_of_participants)]
+    
+
+    def adjusted_talent_luck(self, percentage_talent, percentage_luck):
+        talent= percentage_talent * np.array([i.pull() for i in self.agent])
+        luck= percentage_luck * np.array([i.pull() for i in self.agent])
+        return talent, luck
+    
+
+    def simulation(self):
+        numbers_picked= []
+        for percentage_talent in self.percentage_talent_contribution:
+            talent, luck= self.adjusted_talent_luck(percentage_talent= percentage_talent, percentage_luck= 1-percentage_talent)
+            total_percentage= talent + luck
+            agent_total_score= dict(zip(range(self.total_number_of_participants), total_percentage))
+            agent_talent_score= dict(zip(range(self.total_number_of_participants), talent))
+            ranked_agent_total_score= dict(sorted(agent_total_score.items(), reverse=True, key= lambda x: x[1]))
+            ranked_agent_talent_score= dict(sorted(agent_talent_score.items(), reverse=True, key= lambda x: x[1]))
+            overlap= len(
+                set(list(ranked_agent_total_score.keys())[:self.required_number_of_participants]).intersection(
+                    list(ranked_agent_talent_score.keys())[:self.required_number_of_participants]
+                    ))
+            numbers_picked.append(overlap)
+        
+        return dict(zip(self.percentage_talent_contribution.tolist(), numbers_picked))
+
+
+
+def plot_data(dataframe):
+
+    plt.plot(dataframe)
+    plt.gca().invert_xaxis()
+    plt.xlabel("% Talent Contribution")
+    plt.ylabel("# of Participants Selected")
+
+    plt.tight_layout()
+    plt.show()
